5fd13dd7a7728f9b9f4308d2a8c62a2fde269df6
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-04 10:34:59 +0900

    Add the background.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ZStack(alignment: .top) {
+                Color("BackgroundColor")
+                Image("Background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+
+            }
+        }
     }
 }
 
