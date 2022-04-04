ac3982aa8d4100e0475110b1b0e68a845148da2c
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-04 10:26:18 +0900

    Initial Commit

---
+++ Shared/HuddleLandingPageWithSingleIntroductorySectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuddleLandingPageWithSingleIntroductorySectionApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct HuddleLandingPageWithSingleIntroductorySectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
