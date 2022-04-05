5ae55b06b44516e6bfe24e636d687d8a2469e985
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-05 13:18:08 +0900

    Add the header.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
             VStack(spacing: 30) {
                 Logo()
                 Hero()
+                Header()
             }
             .padding()
         }
---
+++ Shared/Views/Header.swift
@@ -0,0 +1,32 @@
+//
+//  Header.swift
+//  HuddleLandingPageWithSingleIntroductorySection (iOS)
+//
+//  Created by Jinwook Kim on 2022/04/05.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        VStack(spacing: 15) {
+            Text("Build The Community Your Fans Will Love")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.")
+                .font(.title3)
+                .fontWeight(.light)
+                .lineSpacing(5)
+        }
+        .foregroundColor(.white)
+        .multilineTextAlignment(.center)
+    }
+}
+
+struct Header_Previews: PreviewProvider {
+    static var previews: some View {
+        Header()
+            .background(Color("BackgroundColor"))
+            .previewLayout(.sizeThatFits)
+    }
+}
